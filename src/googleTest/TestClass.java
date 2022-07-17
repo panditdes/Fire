f2fa17a2ce997cccb58ba70318b1b82271f568c7
Abhijeet Deshpande <deshpandepandit143@gmail.com>
2022-07-17 14:55:28 +0530

    first commit

---
+++ src/googleTest/TestClass.java
@@ -0,0 +1,42 @@
+package googleTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestClass {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.gecko.driver", "F:\\Pandit\\Required Document\\Selenium\\Firefox\\geckodriver.exe"); // Setting system properties of FirefoxDriver
+				
+	    WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+	    
+		driver.manage().window().maximize();
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.google.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("Browserstack Guide"); //name locator for text box
+		
+		WebElement searchbutton = driver.findElement(By.name("btnK"));//name locator for google search
+		
+		searchbutton.click();
+		
+		driver.quit();
+		
+	    
+
+	}
+
+}
